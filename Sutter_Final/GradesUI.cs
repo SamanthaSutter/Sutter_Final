80a84dc90ed5ecb0dc92adc316cade4e9f2f69e9
Student <Student@IT-DESKTOP>
2023-05-13 18:50:00 -0500

    Add project files.

---
+++ Sutter_Final/GradesUI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sutter_Final
+{
+    class GradesUI
+    {
+        StudentS myStudentS;
+
+        public void MainMethod()
+        {
+            //instance an object of StudentUI..
+            myStudentS = new StudentS();
+            
+            //Call StudentUI->PopulateStudents..
+            //Verify file was successfully read.
+            //if successfull call DisplayInfo()
+            //else display error message.
+
+        }
+
+        void DisplayInfo()
+        {
+            Console.WriteLine("Student id\tLast Name\tAverage  \tGrade");
+
+            for (int index = 0; index < myStudentS.ListLength; index++)
+            {
+
+                Console.WriteLine(" {0} \t {1}    \t {2}    \t {3}", myStudentS.StudentID(index), myStudentS.StudentLastName(index), myStudentS.StudentAverage(index), myStudentS.StudentGrade(index));
+            }
+        }
+    }
+}
---
+++ Sutter_Final/StudentS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sutter_Final
+{
+    class StudentS
+    {
+        List<Student> theStudentList;
+
+
+
+        public string PopulateStudents(string path)
+        {
+            //this is the method you need to implement...NO CONSOLE LOGIC in this file.
+            //use the parameter path for file name, and return an error string!!
+
+            //1) create instance of theStudentList (theStudentList = new List<Student>();)
+            //2) create an instance of the StreamReader Class to read the data from the file named in the variable path
+            //3) Open the File and read line by line using theReadLIne command
+            //4) split the line
+            //5) create one instance of Student, fill in id, first and last through the constructor
+            //6) now loop through remaining fields 3 - 23 for earned and possible and set though method EnterGrade.
+            //7) call CalGrade on the Student object - that sets the average and grade.
+            //8) Add that Student to theStudentList (theStudentList.Add(aStudent);)
+            //9) read next line and loop until null or line = ""
+            //10) Close the file
+
+            //do not forgot to use a Try/Catch block.
+
+
+        }
+
+        public int ListLength
+        {
+            get { return theStudentList.Count; }
+        }
+
+        public string StudentID(int index)
+        {
+            return theStudentList.ElementAt(index).ID;
+        }
+
+        public string StudentLastName(int index)
+        {
+            return theStudentList.ElementAt(index).NameLast;
+        }
+
+        public string StudentGrade(int index)
+        {
+
+            return theStudentList.ElementAt(index).LetterGrade;
+        }
+
+        public float StudentAverage(int index)
+        {
+
+            return theStudentList.ElementAt(index).Average;
+        }
+
+    }
+
+}
+
